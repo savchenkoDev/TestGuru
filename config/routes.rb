c2da94d52673dbc4a15deb7475482c4cf8701683
savchenko <mr.syava1@gmail.com>
2018-07-21 15:22:35 +0500

    Resolve conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :tests, only: :index do
+  resources :tests do
     post :start, on: :member
   end
 
