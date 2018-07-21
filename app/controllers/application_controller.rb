7b7f6a96c70a1e1bd554327527095d78ee8dfd06
savchenko <mr.syava1@gmail.com>
2018-07-21 15:10:39 +0500

    Correction v11.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for
-    current_user.is_a?(Admin) ? admin_tests_path : tests_path
+  before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    admin? ? admin_tests_path : tests_path
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  def admin?
+    current_user.is_a?(Admin)
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
