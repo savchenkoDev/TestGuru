1f815035839030b6e9232d0efc1a060fe40b8ac5
savchenko <mr.syava1@gmail.com>
2018-08-03 21:45:29 +0500

    application.rb config

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TestGuru
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     config.load_defaults 5.2
     config.time_zone = 'Ekaterinburg'
     config.i18n.default_locale = :ru
